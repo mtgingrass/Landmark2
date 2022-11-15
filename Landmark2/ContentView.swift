1622eb78fc0e870d942b9758cec66fa09081ee5c
mtgingrass <mark.gingrass@gmail.com>
2022-11-15 17:59:08 -0500

    Initial Commit

---
+++ Landmark2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Landmark2
+//
+//  Created by Mark Gingrass on 11/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmark2/Landmark2App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark2App.swift
+//  Landmark2
+//
+//  Created by Mark Gingrass on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Landmark2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
