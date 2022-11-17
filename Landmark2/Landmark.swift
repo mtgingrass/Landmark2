4bad0a9ada2dfac05b5a9fbfc6b3a8012274a259
mtgingrass <mark.gingrass@gmail.com>
2022-11-17 16:30:04 -0500

    Creating a list of locations. In progress.

---
+++ Landmark2/Landmark.swift
@@ -0,0 +1,39 @@
+//
+//  Landmark.swift
+//  Park Status
+//
+//  Created by Mark Gingrass on 11/16/22.
+//
+
+import SwiftUI
+import Foundation
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+            var latitude: Double
+            var longitude: Double
+        }
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    
+}
+
---
+++ Landmark2/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  Park Status
+//
+//  Created by Mark Gingrass on 11/17/22.
+//
+
+import Foundation
